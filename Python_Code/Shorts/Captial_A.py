b78939f9fa6aaa64a404add048bc69e2e8c9b27f
Manish Kumar Goswami <53654018+ghostmanish@users.noreply.github.com>
2023-01-29 13:07:30 +0530

    Add files via upload

---
+++ Python_Code/Shorts/Captial_A.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 12:44:34 2023
+
+@author: mgosh
+"""
+import turtle
+
+t = turtle.Turtle()
+
+t.pen()
+
+t.pensize(20)
+
+t.pencolor("red")
+
+t.left(60)
+
+t.forward(300)
+
+t.right(120)
+
+t.forward(300)
+
+t.penup()
+
+t.right(180)
+
+t.forward(150)
+
+t.pendown()
+
+t.left(60)
+
+t.forward(150)
+
+turtle.done()
+
+
